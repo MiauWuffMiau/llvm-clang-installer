d461e4b6de17d51702af19507c11db2c4ab9e796
Marcel Kassuhn <kassuhn@tyntec.com>
2015-06-22 15:26:32 +0200

    Adapted usage output

--- llvm-clang-installer.sh
+++ llvm-clang-installer.sh
@@ -14,12 +14,27 @@ do      case ${option} in
 done
 
 if [ -z "$install_path" ] || [ -z "$build_path" ] || [ -z "$source_path" ]; then
-	echo -e "\nPlease specify an installation path (-I /path/to/installation),"
+	echo -e "\nPlease specify an installation path (-I /path/to/install/dir),"
 	echo -e "a build path (-B /path/to/build/dir)"
 	echo -e "and also the path where the source files shall be saved (-S /path/to/source)!"
 	echo -e "\nTo be on the save side, please provide absolute paths!\n"
+	echo -e "\n-I:"
+	echo -e "\tSpecifies the installation path.\n\tExample: '/path/to/install/dir'"
+	echo -e "\n-B:"
+	echo -e "\tSpecifies the installation path.\n\tExample: '/path/to/build/dir'"
+	echo -e "\n-S:"
+	echo -e "\tSpecifies the installation path.\n\tExample: '/path/to/source/dir'"
+	echo -e "\n-c:"
+	echo -e "\tSpecifies the amount of CPU cores\n\tthe make process is allowed to use.\n\n\tPay attention!"
+	echo -e "\n\tThis will probably result\n\tin a very high load average!\n\n\tExample: -c8"
+	echo -e "\n-d:"
+	echo -e "\tSpecifies whether the installation\n\twill be run as a debug version or not.\n\tExample: -d"
+	echo -e "\n-t:"
+	echo -e "\tSpecifies whether the time programm\n\twill take place to display additional\n\tinformation about execution.\n\tExample: -t\n"
 	exit 1337
 fi
+
+exit 3
 
 if [ -z "$cpu_cores" ]; then
 	cpu_cores="1"
